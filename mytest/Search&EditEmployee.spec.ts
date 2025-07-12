f5051f07ce10c951b0bce0def4d507408fcf333c
Nowshin Tabassum <47469849+Nowshin-Tabassum-Auni@users.noreply.github.com>
2025-07-13 04:35:12 +0600

    Edit Employee Done

--- mytest/Search&EditEmployee.spec.ts
+++ mytest/Search&EditEmployee.spec.ts
@@ -1,6 +1,8 @@
+/* eslint-disable playwright/no-wait-for-timeout */
 import { test, expect } from "@playwright/test";
 import { login } from "../helpers/auth";
 import { generateRandomEmployeeData } from "../helpers/dataGenerator";
+import path from "path";
 
 test.use({
     launchOptions: { slowMo: 2000 },
@@ -14,21 +16,49 @@ test.describe("OrangeHRM Edit Employee Testing", () => {
         // Generate random employee data
         const employee = generateRandomEmployeeData();
 
-        // Navigate via PIM → Add Employee
+        // Navigate to PIM → Add Employee
         await page.getByRole("link", { name: "PIM" }).click();
         await page.getByRole("link", { name: "Add Employee" }).click();
+
         // Create an employee
         await page.getByPlaceholder("First Name").fill(employee.firstName);
         await page.getByPlaceholder("Last Name").fill(employee.lastName);
         await page.getByRole("button", { name: "Save" }).click();
 
-        // Search for the employee and Take a screenshot
+        // Navigate to Employee List → Search
         await page.getByRole("link", { name: "Employee List" }).click();
         await page
             .getByPlaceholder("Type for hints...")
             .first()
             .fill(employee.firstName);
         await page.getByRole("button", { name: "Search" }).click();
+        await page.evaluate(() => window.scrollBy(0, 50));
         await page.screenshot({ path: "test-results/SearchEmployee.png" });
+
+        //Edit the employee
+        await page.getByRole("button", { name: "" }).first().click();
+        await page
+            .locator("div")
+            .filter({ hasText: /^Driver's License NumberLicense Expiry Date$/ })
+            .getByRole("textbox")
+            .first()
+            .fill("DL88857260");
+        await page
+            .locator("form")
+            .filter({ hasText: "Employee Full NameEmployee" })
+            .getByRole("button")
+            .click();
+
+        // Verify employee is updated
+        await expect
+            .soft(
+                page.getByRole("heading", {
+                    name: `${employee.firstName} ${employee.lastName}`,
+                }),
+            )
+            .toBeVisible();
+        // Screenshot
+        await page.waitForTimeout(3000);
+        await page.screenshot({ path: "test-results/EditEmployee.png" });
     });
 });
